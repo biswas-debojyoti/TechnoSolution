ce5c233cae3eb2b5cdd238744c23bcd5a46c44ae
DEBOJYOTI BISWAS <105899504+biswas-debojyoti@users.noreply.github.com>
2026-04-03 18:46:43 +0530

    favicon

--- structured-growth-systems/frontendnext/app/layout.tsx
+++ structured-growth-systems/frontendnext/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         <Header />
-        <main className="flex-1 md:mt-10">
+        <main className="flex-1 md:mt-9">
           {children}
         </main>   
         <AutoContactModal/>
